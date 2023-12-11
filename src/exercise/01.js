9d46a7c5ed956a3ae97f365a0d0a9f95d3b8df30
Martin-Luther Obahor <kingm4159@gmail.com>
2023-12-11 02:49:29 +0100

    Lesson 1 : UseReducer Extra Credit -  traditional dispatch object with a type and switch statement

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,19 +3,26 @@
 
 import * as React from 'react'
 function countReducer(state, action) {
-  return {
-    ...state,
-    ...(typeof action === "function" ? action(state) : action)
+  // return {
+  //   ...state,
+  //   ...(typeof action === "function" ? action(state) : action)
+  // }
+  switch (action.type) {
+    case "INCREMENT" : {
+      return { count : state.count + action.step}
+    }
+    default: {
+      throw new Error(`Unsupported Action Type ${action.type}`)
+    }
   }
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [state, setState] = React.useReducer(countReducer, {
-    count: initialCount,
-  })
+  const [state, dispatch] = React.useReducer(countReducer, {
+  count: initialCount,
+})
   const {count} = state
-  const increment = () =>
-    setState(currentState => ({count: currentState.count + step}))
+  const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
