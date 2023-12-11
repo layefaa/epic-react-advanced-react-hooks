5acfd9a664d9f67783c78529c061cca7fa8d663f
Martin-Luther Obahor <kingm4159@gmail.com>
2023-12-11 02:33:57 +0100

    Lesson 1 : UseReducer Extra Credit - simulate setState with an object

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,13 +2,19 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-function countReducer(state, newState) {
-  return state + newState
+function countReducer(state, action) {
+  return {
+    ...state,
+    ...action
+  }
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [count, changeCount] = React.useReducer(countReducer, initialCount)
-  const increment = () => changeCount(step)
+  const [state, setState] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
+  const {count} = state
+  const increment = () => setState({count: count + step})
   return <button onClick={increment}>{count}</button>
 }
 
